eb4ca6a71edaa6694115a4073ba4f3ce25c857a1
Adam Khodier <khodieradam1297@gmail.com>
2022-11-27 17:04:49 -0500

    commit tree 794206d5bb72dc2f07ff36a3cbe0d26c53873e08

---
+++ db/migrate/20221127220341_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :string
+      t.string :last_name
+      t.string :string
+      t.integer :grade
+
+      t.timestamps
+    end
+  end
+end
